c2d20db622f381266d0841daea318ac5980a4a5e
Artur Ksybek <artur.ksybek@telestrada.pl>
2021-07-16 16:03:44 +0200

    (feature): pagintion is works

--- src/components/StartView/StartView.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <h1>Welcome in Planets App!!</h1>
-    <button @click="getData">Show planets</button>
-    <PlanetsLayout v-if="dataObject" v-bind:data-object="dataObject"/>
-  </div>
-</template>
-
-<script src="./StartView.js"/>
