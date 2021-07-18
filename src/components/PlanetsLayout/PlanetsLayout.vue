42f578b34f79007d849939371f1de9d9509ae047
Artur Ksybek <ar2r111@gmail.com>
2021-07-18 21:52:22 +0200

    (inital): loader, pagination

--- src/components/PlanetsLayout/PlanetsLayout.vue
+++ src/components/PlanetsLayout/PlanetsLayout.vue
@@ -3,13 +3,17 @@
     <h1>Welcome in Planets App!!</h1>
 
     <button @click="ShowOrHidePlanetsLayout">{{buttonTitle}}</button>
+
+    <Loading v-if="isLoading"/>
     
     <transition name="fade">
       <Pagination v-if="showPlanets" v-bind:links-arr="linksArr" @getDataByLink="getData" />
     </transition>
 
+    <SearchInput v-if="planetsData"/>
+
     <transition name='fade'>
-      <ul v-if="showPlanets" class="planetsList">
+      <ul v-if="planetsData" class="planetsList">
         <Planet v-for="(planetData, i) in planetsData.results" v-bind:key="i" v-bind:planet-data="planetData"/>
       </ul>
     </transition>
